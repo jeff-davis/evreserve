a464060e3a6d2cee4dc640cad2e5f56c019b5353
Jeff Davis <jdavis@postgresql.org>
2014-04-02 22:35:10 -0700

    Work around rust issue #13246.

--- src/app.rs
+++ src/app.rs
@@ -9,13 +9,11 @@ pub fn get_content() -> ~str {
 	content.push_str("<html><head><title>Trying Rust!</title></head><body>\n");
 	content.push_str("<ul>\n");
 
-  {
-    let conn = PostgresConnection::connect("postgres://jdavis@localhost:5432/postgres", &NoSsl).unwrap();
-    let stmt = conn.prepare("SELECT x FROM t WHERE $1").unwrap();
-    for row in stmt.query([&true as &ToSql]).unwrap() {
-      let val : i32 = row[1];
-      content.push_str(format!("<li>{}</li>\n", val));
-    }
+  let conn = PostgresConnection::connect("postgres://jdavis@localhost:5432/postgres", &NoSsl).unwrap();
+  let stmt = conn.prepare("SELECT x FROM t").unwrap();
+  for row in stmt.query([]).unwrap() {
+    let val : i32 = row[1];
+    content.push_str(format!("<li>{}</li>\n", val));
   }
 
   content.push_str("</ul>\n");
